df830ef4b6e016c6afad2d8f5e5e6893d2e21503
Your Name <youremail@example.com>
2025-04-16 16:20:24 +0530

    linked the buttons

--- src/main/java/com/example/demo/government_tax_department_system/Item.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo.government_tax_department_system;
-
-public class Item {
-}
---
+++ src/main/java/com/example/demo/government_tax_department_system/Main.java
@@ -0,0 +1,23 @@
+package com.example.demo.government_tax_department_system;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/demo/government_tax_department_system/main.fxml")));
+        primaryStage.setTitle("Government Tax Department System");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
